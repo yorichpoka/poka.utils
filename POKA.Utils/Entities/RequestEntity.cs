ab79a5cb1fcb43d0795e0ce25a3a1891bf526b7b
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-21 09:48:17 +0200

    Update package

--- POKA.Utils/Entities/RequestEntity.cs
+++ POKA.Utils/Entities/RequestEntity.cs
@@ -4,18 +4,19 @@ using POKA.Utils.Enums;
 
 namespace POKA.Utils.Entities
 {
-    public class RequestEntity : BaseEntity<RequestId>, IHasCreatedOn
+    public class RequestEntity : BaseEntity<RequestId>, IHasCreatedOn, IHasCreatedByUserId
     {
         public string ApplicationPerformer { get; private set; } = null!;
         public RequestStatusEnum Status { get; private set; } = null!;
+        public RequestScopeId ScopeId { get; private set; } = null!;
         public RequestTypeEnum Type { get; private set; } = null!;
         public string Name { get; private set; } = null!;
         public string Data { get; private set; } = null!;
-        public string? Error { get; private set; }
         public DateTime CreatedOn { get; private set; }
-        public TimeSpan? Duration { get; private set; }
+        public UserId? CreatedByUserId { get; private set; }
         public RequestId? ParentId { get; private set; }
-        public RequestScopeId? ScopeId { get; private set; }
+        public TimeSpan? Duration { get; private set; }
+        public string? Error { get; private set; }
 
         private RequestEntity()
         {
@@ -23,38 +24,40 @@ namespace POKA.Utils.Entities
 
         public RequestEntity(RequestId id)
         {
-            this.Id = id;
+            Id = id;
         }
 
-        public RequestEntity(RequestScopeId? scopeId, RequestId id, string applicationPerformer, RequestStatusEnum status, string name, string data, DateTime createdOn, RequestId? parentId = null)
+        public RequestEntity(
+            RequestScopeId scopeId, 
+            string applicationPerformer,
+            string name, 
+            string data, 
+            DateTime createdOn, 
+            UserId? createdByUserId = null, 
+            RequestId? parentId = null
+        )
         {
+            Id = BaseObjectId.Create<RequestId>();
+            Status = RequestStatusEnum.Pending;
+            Type = RequestTypeEnum.Query;
             ApplicationPerformer = applicationPerformer;
+            CreatedByUserId = createdByUserId;
             CreatedOn = createdOn;
             ParentId = parentId;
             ScopeId = scopeId;
             Duration = null;
-            Status = status;
             Error = null;
-            Type = null;
             Name = name;
             Data = data;
-            Id = id;
         }
 
-        public void AsCommand()
-        {
-            this.Type = RequestTypeEnum.Command;
-        }
+        public void AsCommand() => this.Type = RequestTypeEnum.Command;
 
-        public void AsQuery()
-        {
-            this.Type = RequestTypeEnum.Query;
-        }
+        public void AsQuery() => this.Type = RequestTypeEnum.Query;
 
         public void Success()
         {
             this.Status = RequestStatusEnum.Success;
-
             this.Complete();
         }
 
@@ -62,13 +65,9 @@ namespace POKA.Utils.Entities
         {
             this.Status = RequestStatusEnum.Fail;
             this.Error = error;
-
             this.Complete();
         }
 
-        private void Complete()
-        {
-            this.Duration = DateTime.UtcNow - this.CreatedOn;
-        }
+        private void Complete() => this.Duration = DateTime.UtcNow - this.CreatedOn;
     }
 }
