234224ec0eb841d517287eb7a0ee1f661cf3fa51
ulrichpoka <ulrich.poka@technologypartner.io>
2024-07-20 11:12:47 +0200

    create new project

---
+++ src/windowService/tests/POKA.POC.WindowsService.WindowsService.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("POKA.POC.WindowsService.WindowsService.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("POKA.POC.WindowsService.WindowsService.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ec287c3-1b3d-472b-94aa-1c01ac730b9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/windowService/tests/POKA.POC.WindowsService.WindowsService.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace POKA.POC.WindowsService.WindowsService.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
