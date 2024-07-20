a3eea3d0677eb83d9076f5d6f620176a778f01a3
ulrichpoka <ulrich.poka@technologypartner.io>
2024-07-20 11:26:17 +0200

    improvement

--- src/windowService/src/POKA.POC.WindowsService.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace POKA.POC.WindowsService.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/windowService/src/POKA.POC.WindowsService.Domain/Entities/ItemEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace POKA.POC.WindowsService.Domain.Entities
+{
+    public class ItemEntity
+    {
+        public Guid Id { get; private set; }
+        public object Value { get; private set; }
+
+        public ItemEntity(object value)
+        {
+            Id = Guid.NewGuid();
+            Value = value;
+        }
+    }
+}
