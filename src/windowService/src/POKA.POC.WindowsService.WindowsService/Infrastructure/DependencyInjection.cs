6db451f4f84a4cc3f3bdb42c8456fb84f49a86f3
ulrichpoka <ulrich.poka@technologypartner.io>
2024-07-20 12:42:39 +0200

    improvement

--- src/windowService/src/POKA.POC.WindowsService.WindowsService/Infrastructure/DependencyInjection.cs
+++ src/windowService/src/POKA.POC.WindowsService.WindowsService/Infrastructure/DependencyInjection.cs
@@ -2,8 +2,13 @@
 using POKA.POC.WindowsService.WindowsService.Application.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Unity.Microsoft.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog.Extensions.Logging;
 using System.Reflection;
+using Unity.Injection;
 using Unity.Lifetime;
+using Serilog;
+using System;
 using Unity;
 
 namespace POKA.POC.WindowsService.Extensions
@@ -14,6 +19,7 @@ namespace POKA.POC.WindowsService.Extensions
         {
             unityContainer
                 .RegisterType<IAppSettingsProvider, AppSettingsProvider>(new SingletonLifetimeManager())
+                .AddLogger()
                 .AddMediatR();
 
             return unityContainer;
@@ -30,6 +36,35 @@ namespace POKA.POC.WindowsService.Extensions
                                         );
 
             unityContainer.BuildServiceProvider(serviceCollection);
+
+            return unityContainer;
+        }
+
+        private static IUnityContainer AddLogger(this IUnityContainer unityContainer)
+        {
+            Log.Logger = new LoggerConfiguration()
+                            .WriteTo
+                                .File(
+                                    path: $"logs/{DateTime.UtcNow:dd-MM-yyyy}.json"
+                                )
+                            .CreateLogger();
+
+            unityContainer
+                .RegisterInstance<ILoggerFactory>(new SerilogLoggerFactory(), new SingletonLifetimeManager())
+                .RegisterInstance(
+                    t: typeof(ILogger<>), 
+                    instance: new InjectionFactory(
+                        (_unityContainer, type, name) =>
+                        {
+                            var loggerFactory = _unityContainer.Resolve<ILoggerFactory>();
+                            var loggerType = type.GenericTypeArguments[0];
+                            var logger = loggerFactory.CreateLogger(loggerType);
+
+                            return logger;
+                        }
+                    ),
+                    lifetimeManager: new SingletonLifetimeManager()
+                );
 
             return unityContainer;
         }
---
+++ src/windowService/tests/POKA.POC.WindowsService.WindowsService.Test/Application/Commands/CreateAndLogItemCommandTest.cs
@@ -0,0 +1,26 @@
+﻿using POKA.POC.WindowsService.WindowsService.Application.Commands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MediatR;
+using System;
+using Unity;
+
+namespace POKA.POC.WindowsService.WindowsService.Test.Application.Commands
+{
+    [TestClass]
+    public class CreateAndLogItemCommandTest : BaseTest
+    {
+        [TestMethod]
+        public void CanExecuteCommand()
+        {
+            // Arrange
+            var mediator = this._unityContainer.Resolve<IMediator>();
+            var createAndLogItemCommand = new CreateAndLogItemCommand(DateTime.UtcNow.ToString());
+
+            // Act
+            var result = mediator.Send(createAndLogItemCommand);
+
+            // Assert
+            Assert.AreNotEqual(result, Guid.Empty);
+        }
+    }
+}
